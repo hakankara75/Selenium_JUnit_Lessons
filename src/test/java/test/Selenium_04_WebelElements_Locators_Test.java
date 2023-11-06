37ec802612e922f3b04ab363256990a47def8606
HakanKara75 <hakanbatirhan@gmail.com>
2023-11-06 11:32:01 +0300

    v

--- src/test/java/test/Selenium_04_WebelElements_Locators_Test.java
+++ src/test/java/test/Selenium_04_WebelElements_Locators_Test.java
@@ -19,7 +19,25 @@ public class Selenium_04_WebelElements_Locators_Test {
         Thread.sleep(2000);
         driver.manage().window().maximize();
 
+        WebElement id_SearchBox= driver.findElement(By.id("twotabsearchtextbox"));
+        id_SearchBox.click();
 
+        WebElement xpath_SearchBox= driver.findElement(By.xpath("//input[@placeholder='Search Amazon']"));
 
+        WebElement cssSelector_English= driver.findElement(By.cssSelector("span[class='icp-nav-link-inner']"));
+
+        WebElement name_SearchBox= driver.findElement(By.name("field-keywords"));
+
+        WebElement className_SearchBox=driver.findElement(By.className("nav-progressive-attribute"));
+
+        WebElement tagName_Header= driver.findElement(By.tagName("header"));
+
+        WebElement linkText_English= driver.findElement(By.linkText("EN"));
+        linkText_English.click();
+
+        WebElement partialLinkText_English=driver.findElement(By.partialLinkText("Hello, sign in"));
+        Thread.sleep(2000);
+
+        driver.close();
     }
 }
