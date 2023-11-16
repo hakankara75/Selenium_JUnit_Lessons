ba460105a5fe234f97ad1cb573d09e77ad12b914
hakankara75 <hakanbatirhan@gmail.com>
2023-11-16 22:54:45 +0300

    v

---
+++ src/test/java/Topics/Selenium_05_JUnit.java
@@ -0,0 +1,32 @@
+package Topics;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class Selenium_05_JUnit {
+    /*
+    Test Anatationlari bizim test yapapilecegimiz
+    Testimizin gecip kaldigini gosteren
+    Main metoda ihtiyac birakmayan kod bloklaridir
+    @Before  ==> Her bir @Test'ten once calisir
+    @After   ==> Her bir @Test'ten sonre calisir
+    @BeforeClass ==> Her bir classdan once calisir
+    @AfterClass  ==> Her bir classdan sonra calisir
+    @Ignore    ==> Calismasini istemedigimiz kodlarin kodlarin basina koyulur
+    @Test   ==> Test kodlarinin basina koyulur.
+    Anatotion: Bir veri hakkinda bilgi barindiran meta datalardir.
+    Javadaki compiler gibi gorev yapar.
+     */
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void name() {
+        System.out.println("Bu test metodudur");
+    }
+    @Ignore
+    public void ignore() {
+        System.out.println("Bu test metodudur");
+    }
+}
